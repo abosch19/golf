2e4c9f2ff25aa3a724a28802a8c0673a2ae65a49
Alex Bosch <abosch19@gmail.com>
2025-07-01 09:56:38 +0200

    Refactor loading state in PublicLayout component: Replaced the default loading message with a Loading component to enhance user experience and maintain consistent UI design across the application.

--- src/modules/user/layout/PublicLayout.tsx
+++ src/modules/user/layout/PublicLayout.tsx
@@ -1,12 +1,13 @@
 // src/layouts/PrivateLayout.tsx
 import { Navigate, Outlet } from "react-router";
+import { Loading } from "@/components/layouts/Loading";
 import { Paths } from "@/utils/paths";
 import { useUser } from "../context/UserContext";
 
 export default function PublicLayout() {
 	const { user, isLoading } = useUser();
 
-	if (isLoading) return <div>Loading...</div>;
+	if (isLoading) return <Loading />;
 	if (user) return <Navigate to={Paths.HOME} />;
 
 	return <Outlet />;
